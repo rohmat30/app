8f4271b587c46c913cf3c584769f4041eab607de
MuhammadRohmat <rohmatif30@gmail.com>
2018-06-13 15:32:29 +0700

    fitur login tahap pertama

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('promise-mysql');
+
+bcrypt = require('bcrypt');
+
+sql = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ewarga'
+});
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +25,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({secret: 'ewarga',resave: true, saveUninitialized: true}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var Autentikasi = require('../mc/autentikasi');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',async function(req, res) {
+  if (req.session.username) {
+    res.redirect('/users');
+  } else {
+    res.render('index', { title: 'Login'});
+  }
+}).post('/',async function(req, res){
+  let username = req.body.username,
+      password = req.body.password;
+  let auth = await Autentikasi.login(username,password);
+
+  if (auth.err) {
+    res.render('index', { title:'Login', err: auth.msg });
+  } else {
+    req.session.username = username;
+    res.redirect('/users');
+  }
+})
+
+router.get('/logout',function(req, res){
+  Autentikasi.logout(req,res);
 });
 
+router.all('*',function(req, res, next){
+  if (!req.session.username) {
+    res.redirect('/');
+  }
+  next();
+});
 module.exports = router;
